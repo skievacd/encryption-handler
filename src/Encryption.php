3abad5f1c24b35d1ba9d766d2ac0005bec5eb551
Carlos Skieva <carlos.skieva@pinbus.com>
2021-03-26 11:08:28 -0300

    first commit

---
+++ src/Encryption.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace EncryptionHandler\Encryption;
+
+class Encryption 
+{
+    protected $key;
+    protected $method;
+    protected $data;
+    protected $iv;
+
+    function __construct($data, $key = '', $method = 'AES-256-CBC', $InitialVector = '') {
+        $this->data = $data;
+        $this->key = $this->passwordDeriveBytes($key, null);
+        $this->method = $method;
+        $this->iv = $InitialVector;
+    }
+
+    function passwordDeriveBytes($password, $salt, $iterations = 100, $len = 32) {
+        $key = $password . $salt;
+        for($i = 0; $i < $iterations; $i++) {
+            $key = sha1($key, true);
+        }
+        if (strlen($key) < $len) {
+            $hx = $this->passwordDeriveBytes($password, $salt, $iterations - 1, 20);
+            $counter = 0;
+            while (strlen($key) < $len) {
+                $counter += 1;
+                $key .= sha1($counter . $hx, true);
+            }
+        }
+        return substr($key, 0, $len);
+    }
+
+    function encrypt(): string {
+        return openssl_encrypt($this->data, "aes-256-cbc", $this->key, 0, $this->iv);
+    }
+
+    function decrypt(): string {
+        return openssl_decrypt($this->data, "aes-256-cbc", $this->key, 0, $this->iv);
+    }
+}
